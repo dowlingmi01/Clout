360a3a1dc031ef6ab4a6a8fa03ccd23d24abbe74
Adriano <adrianochuyt@hotmail.com>
2022-09-15 14:18:41 -0600

    modified profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :completed_surveys, through: :completions
   has_many :enrollments
   has_one :profile
+  validates :first_name, :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
